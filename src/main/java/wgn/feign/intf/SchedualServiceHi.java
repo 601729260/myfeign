f87f757c7c791d332e4b6598c5766bfae6786b51
601729260 <601729260@163.com>
2018-04-23 17:53:52 +0800

    增加断路器功能

--- src/main/java/wgn/feign/intf/SchedualServiceHi.java
+++ src/main/java/wgn/feign/intf/SchedualServiceHi.java
@@ -5,10 +5,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import wgn.feign.impl.SchedualServiceHiHystric;
+
 /**
 * Created by fangzhipeng on 2017/4/6.
 */
-@FeignClient(value = "service-hi")
+@FeignClient(value = "service-hi",fallback= SchedualServiceHiHystric.class)
 public interface SchedualServiceHi {
 @RequestMapping(value = "/hi",method = RequestMethod.GET)
 String sayHiFromClientOne(@RequestParam(value = "name") String name);
